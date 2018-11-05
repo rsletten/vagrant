75dfcb719dbdb8caf07aa76118aa11798c53f05c
Rob Sletten <rsletten@gmail.com>
2018-11-05 15:52:10 -0500

    features

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,4 +1,11 @@
 # default.pp
 node default {
   include bla
+  include stdlib
+  logrotate::rule { 'messages':
+    path         => '/var/log/messages',
+    rotate       => 5,
+    rotate_every => 'week',
+    postrotate   => '/usr/bin/killall -HUP syslogd',
+  }
 }
